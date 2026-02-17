a5db02777943386ada913c9470f91d52d6349943
FormulaCarbon <99456163+FormulaCarbon@users.noreply.github.com>
2026-02-16 21:56:18 -0600

    fized carousel and added stuff to main page

---
+++ scripts/colormode.js
@@ -0,0 +1,3 @@
+// Source - https://stackoverflow.com/a/63087710
+// Posted by Carol Skelly, modified by community. See post 'Timeline' for change history
+// Retrieved 2026-02-16, License - CC BY-SA 4.0
